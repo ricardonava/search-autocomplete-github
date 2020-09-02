6185d0df1712e91efd40ba835f2d5757219e8e79
Ricardo <ricardo.a.nava@gmail.com>
2020-09-01 22:42:41 -0700

    Refactor Issues component to add labels

--- src/components/Issues.jsx
+++ src/components/Issues.jsx
@@ -7,7 +7,21 @@ export default function Searchbar({ issues }) {
         {issues.map((issue) => (
           <li key={issue._id}>
             <a className="title" href={issue.url}>
-              <div className="issue">{issue.title}</div>
+              <div className="issue">
+                <div className="issue-status">{issue.state}</div>
+                <div className="issue-title">{issue.title}</div>
+                <div className="issue-labels">
+                  {issue.labels.map((label) => (
+                    <span
+                      className="issue-label"
+                      key={label.id}
+                      style={{ backgroundColor: `#${label.color}` }}
+                    >
+                      {label.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </a>
           </li>
         ))}
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Searchbar() {
   return (
     <div>
-      <input type="text" placeholder="Search.." />
+      <input type="text" placeholder="Search..." />
     </div>
   );
 }
