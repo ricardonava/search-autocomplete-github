323bc245e7aebcb4206c2813f722b7be67ef71b4
Ricardo <ricardo.a.nava@gmail.com>
2020-09-04 01:25:12 -0700

    Refactor app to restyle

--- src/components/Issues.jsx
+++ src/components/Issues.jsx
@@ -3,31 +3,34 @@ import contrastText from './../utils/contrastText'
 
 export default function Searchbar({ issues }) {
   return (
-    <ul className="issues">
-      {issues.map((issue) => (
-        <li key={issue._id}>
-          <a className="title" href={issue.url}>
-            <div className="issue">
-              <div className="issue-status">{issue.state}</div>
-              <div className="issue-title">{issue.title}</div>
-              <div className="issue-labels">
-                {issue.labels.map((label) => (
-                  <span
-                    className="issue-label"
-                    key={label.id}
-                    style={{
-                      backgroundColor: `#${label.color}`,
-                      color: `${contrastText(label.color)}`,
-                    }}
-                  >
-                    {label.name}
-                  </span>
-                ))}
+    <div>
+      <h1>Latest open issues...</h1>
+      <ul className="issues">
+        {issues.map((issue) => (
+          <li key={issue._id}>
+            <a className="title" href={issue.url}>
+              <div className="issue">
+                <div className="issue-status">{issue.state}</div>
+                <div className="issue-title">{issue.title}</div>
+                <div className="issue-labels">
+                  {issue.labels.map((label) => (
+                    <div
+                      className="issue-label"
+                      key={label.id}
+                      style={{
+                        backgroundColor: `#${label.color}`,
+                        color: `${contrastText(label.color)}`,
+                      }}
+                    >
+                      {label.name}
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          </a>
-        </li>
-      ))}
-    </ul>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
