6c35d20b57ab13b963c4564dd0197fea64d74a0a
Ricardo <ricardo.a.nava@gmail.com>
2020-09-03 20:31:12 -0700

    Remove extra divs

--- src/components/Issues.jsx
+++ src/components/Issues.jsx
@@ -3,33 +3,31 @@ import contrastText from './../utils/contrastText'
 
 export default function Searchbar({ issues }) {
   return (
-    <div>
-      <ul className="issues">
-        {issues.map((issue) => (
-          <li key={issue._id}>
-            <a className="title" href={issue.url}>
-              <div className="issue">
-                <div className="issue-status">{issue.state}</div>
-                <div className="issue-title">{issue.title}</div>
-                <div className="issue-labels">
-                  {issue.labels.map((label) => (
-                    <span
-                      className="issue-label"
-                      key={label.id}
-                      style={{
-                        backgroundColor: `#${label.color}`,
-                        color: `${contrastText(label.color)}`,
-                      }}
-                    >
-                      {label.name}
-                    </span>
-                  ))}
-                </div>
+    <ul className="issues">
+      {issues.map((issue) => (
+        <li key={issue._id}>
+          <a className="title" href={issue.url}>
+            <div className="issue">
+              <div className="issue-status">{issue.state}</div>
+              <div className="issue-title">{issue.title}</div>
+              <div className="issue-labels">
+                {issue.labels.map((label) => (
+                  <span
+                    className="issue-label"
+                    key={label.id}
+                    style={{
+                      backgroundColor: `#${label.color}`,
+                      color: `${contrastText(label.color)}`,
+                    }}
+                  >
+                    {label.name}
+                  </span>
+                ))}
               </div>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+            </div>
+          </a>
+        </li>
+      ))}
+    </ul>
   )
 }
